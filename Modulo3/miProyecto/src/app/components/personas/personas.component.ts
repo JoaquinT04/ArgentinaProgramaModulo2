21196217677ae455076286dfc547ae7a24ce67fb
Joaquin Tejerina <joaquintejerina19@hotmail.com>
2023-01-11 18:12:25 -0300

    Trabajando con los componentes de personas y personas

--- Modulo3/miProyecto/src/app/components/personas/personas.component.ts
+++ Modulo3/miProyecto/src/app/components/personas/personas.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+import { Persona } from "src/app/models/Persona";
 @Component({
   selector: 'app-personas',
   templateUrl: './personas.component.html',
   styleUrls: ['./personas.component.css']
 })
-export class PersonasComponent {
+
+
+export class PersonasComponent implements OnInit{
+
+  // atributos
+  personas:Persona[] = []
+
+  // constructor
+  constructor(){
+
+  }
+
+  // comportamiento
+
+  ngOnInit(): void {
+    let persona1 = new Persona("Joaquin","Tejerina",24)
+    this.personas.push(persona1)
+    this.personas.push(new Persona("Jorge","Perez",21))
+    this.personas.push(new Persona("Niki","Nicol",22))
+  }
 
 }
